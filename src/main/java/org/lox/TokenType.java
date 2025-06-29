9354516059083b3f3dbd44ddc7c72184c6505bbd
Pedro Mateus <pedropmateus91@gmail.com>
2025-06-29 17:57:08 +0100

    Add TokenType enum

---
+++ src/main/java/org/lox/TokenType.java
@@ -0,0 +1,29 @@
+package org.lox;
+
+enum TokenType {
+  // single-character tokens
+  LEFT_PAREN, RIGHT_PAREN,
+  LEFT_BRACE, RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS, PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+
+  // one or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_THAN,
+  LESS, LESS_EQUAL,
+
+  // Literals
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  // keywords
+  AND, CLASS, ELSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  EOF
+}
